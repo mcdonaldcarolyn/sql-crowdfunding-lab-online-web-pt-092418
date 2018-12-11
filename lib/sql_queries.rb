05c2d86997480119215006d9eebb060ddb7d97e6
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-11 10:37:09 -0500

    Interim checkin

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT project_id, title, SUM(amount) FROM pledges EXCEPT SELECT id, funding_goal  FROM projects WHERE pledges.amount > projects.funding_goal"
+"SELECT project_id, SUM(amount) FROM pledges INNER JOIN projects ON id, funding_goal WHERE pledges.amount > projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
